288e8fec0a5430b2ea6fd29dea2809ce58af8442
Amy <amy+git@amogus.cloud>
2025-12-24 15:04:53 -0400

    wip: start working on custom css impl w/ monaco
    Known issues with current impl:
        - Changes happen instantly, which causes stutters while editing
        - There are no classes for a lot of components, so the feature by
        itself is useless
        - Monaco does not follow system preference
        - Monaco is not lazy loaded, im going to implement it sometime soon.
        It is not a priority at the moment.
    
    Signed-off-by: Amy <amy+git@amogus.cloud>

---
+++ packages/client/components/ui/components/features/monaco/Monaco.tsx
@@ -0,0 +1,60 @@
+import { onCleanup, onMount } from "solid-js";
+import * as monaco from "monaco-editor";
+import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker';
+import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
+
+import { cva } from "styled-system/css";
+
+type Props = {
+  defaultValue: string
+  onChange?: (event: monaco.editor.IModelContentChangedEvent, editor: monaco.editor.IModel | null) => void
+}
+
+self.MonacoEnvironment = {
+  getWorker(_, label) {
+    if (label == "css") {
+      return new cssWorker()
+    }
+
+    return new editorWorker()
+  }
+}
+
+const monacoStyle = cva({
+  base: {
+    width: "100%",
+    height: "600px",
+    border: "1px solid #ccc"
+  }
+})
+
+export function MonacoEditor(props: Props) {
+  let element!: HTMLDivElement
+  let editor: monaco.editor.IStandaloneCodeEditor;
+  let disposables: (monaco.IDisposable | undefined)[] = [];
+
+  onMount(() => {
+    if (element?.isConnected) {
+        editor = monaco.editor.create(element, {
+            value: props.defaultValue,
+            language: 'css',
+        })
+
+      if (props.onChange) {
+        disposables.push(editor.getModel()?.onDidChangeContent((ev) => props.onChange!(ev, editor.getModel())));
+      }
+    }
+  })
+
+  onCleanup(() => {
+    for (const disposable of disposables) {
+      if (!disposable) continue;
+
+      disposable.dispose()
+    }
+
+    editor.dispose();
+  })
+
+  return <div class={"monaco " + monacoStyle()} ref={element} />
+}
