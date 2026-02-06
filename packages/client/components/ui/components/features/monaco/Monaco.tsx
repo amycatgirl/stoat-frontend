4561db6bf0d9f83ea3dc307d66271bab67f5d337
Amy <amy+git@amogus.cloud>
2026-02-06 15:27:28 -0400

    chore: lazy load editor
    Signed-off-by: Amy <amy+git@amogus.cloud>

--- packages/client/components/ui/components/features/monaco/Monaco.tsx
+++ packages/client/components/ui/components/features/monaco/Monaco.tsx
@@ -1,60 +1,72 @@
-import { onCleanup, onMount } from "solid-js";
 import * as monaco from "monaco-editor";
-import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker';
-import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
+import editorWorker from "monaco-editor/esm/vs/editor/editor.worker?worker";
+import cssWorker from "monaco-editor/esm/vs/language/css/css.worker?worker";
+import { onCleanup, onMount } from "solid-js";
 
 import { cva } from "styled-system/css";
 
 type Props = {
-  defaultValue: string
-  onChange?: (event: monaco.editor.IModelContentChangedEvent, editor: monaco.editor.IModel | null) => void
-}
+  defaultValue: string;
+  onChange?: (
+    event: monaco.editor.IModelContentChangedEvent,
+    editor: monaco.editor.IModel | null,
+  ) => void;
+};
 
 self.MonacoEnvironment = {
   getWorker(_, label) {
     if (label == "css") {
-      return new cssWorker()
+      return new cssWorker();
     }
 
-    return new editorWorker()
-  }
-}
+    return new editorWorker();
+  },
+};
 
 const monacoStyle = cva({
   base: {
     width: "100%",
     height: "600px",
-    border: "1px solid #ccc"
-  }
-})
+    border: "1px solid #ccc",
+  },
+});
 
-export function MonacoEditor(props: Props) {
-  let element!: HTMLDivElement
+/**
+ * Monaco Editor wrapper
+ */
+export default function MonacoEditor(props: Props) {
+  let element!: HTMLDivElement;
   let editor: monaco.editor.IStandaloneCodeEditor;
-  let disposables: (monaco.IDisposable | undefined)[] = [];
+  const disposables: (monaco.IDisposable | undefined)[] = [];
 
   onMount(() => {
     if (element?.isConnected) {
-        editor = monaco.editor.create(element, {
-            value: props.defaultValue,
-            language: 'css',
-        })
+      editor = monaco.editor.create(element, {
+        value: props.defaultValue,
+        language: "css",
+      });
 
       if (props.onChange) {
-        disposables.push(editor.getModel()?.onDidChangeContent((ev) => props.onChange!(ev, editor.getModel())));
+        disposables.push(
+          editor
+            .getModel()
+            ?.onDidChangeContent((ev) =>
+              props.onChange!(ev, editor.getModel()),
+            ),
+        );
       }
     }
-  })
+  });
 
   onCleanup(() => {
     for (const disposable of disposables) {
       if (!disposable) continue;
 
-      disposable.dispose()
+      disposable.dispose();
     }
 
     editor.dispose();
-  })
+  });
 
-  return <div class={"monaco " + monacoStyle()} ref={element} />
+  return <div class={"monaco " + monacoStyle()} ref={element} />;
 }
